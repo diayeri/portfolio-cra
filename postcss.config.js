bf200cb2010d11f825e43578e71f62e25e012ff7
dia24 <dia24@estsoft.com>
2025-05-15 18:00:46 +0900

    fix: ES 모듈 형식으로 구성 파일 변환
    - postcss.config.js와 tailwind.config.js를 ES 모듈 형식(export default)으로 변경
    - 타입 "module" 설정과의 호환성 문제 해결
    - ToggleSwitch 컴포

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcssPlugin from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcssPlugin,
+    autoprefixer,
+  ],
 };
